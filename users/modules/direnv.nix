bb976898e777844b03667f1219cc98113b943546
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-05-15 16:12:57 +0200

    feat(direnv): less verbose direnv

--- users/modules/direnv.nix
+++ users/modules/direnv.nix
@@ -1,7 +1,10 @@
 {
   programs.direnv = {
     enable = true;
-    config.global.load_dotenv = true;
+    config.global = {
+      load_dotenv = true;
+      hide_env_diff = true;
+    };
     nix-direnv.enable = true;
   };
 }
