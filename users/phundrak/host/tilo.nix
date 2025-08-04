50ebd68e57eb748ea64409c0297071b3f3b5eccc
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-08-04 21:39:49 +0200

    fix: correct values for host Tilo

--- users/phundrak/host/tilo.nix
+++ users/phundrak/host/tilo.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   imports = [../light-home.nix];
   home = {
-    cli.nh.flake = "${config.home.homeDirectory}/nixos";
+    cli.nh.flake = "/tank/phundrak/nixos";
     phundrak.sshKey = {
       content = builtins.readFile ../../../keys/id_tilo.pub;
       # file = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
