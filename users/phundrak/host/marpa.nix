ee0bc337e6e384adaab3e631149e4616159eaa7d
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-08-06 20:30:18 +0200

    fix(marpa): fix location of dotfiles

--- users/phundrak/host/marpa.nix
+++ users/phundrak/host/marpa.nix
@@ -1,11 +1,10 @@
 {config, ...}: {
   imports = [../home.nix];
   home = {
-    cli.nh.flake = "${config.home.homeDirectory}/nixos";
+    cli.nh.flake = "${config.home.homeDirectory}/.dotfiles";
     desktop.hyprland.host = "marpa";
     phundrak.sshKey = {
       content = builtins.readFile ../../../keys/id_marpa.pub;
-      # file = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
     };
   };
 }
