a67a4f5826a1422bd4ed078e9ebe147cb4a54be5
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-08-07 11:07:01 +0200

    fix(gampo): correctly set dotfiles location

--- users/phundrak/host/gampo.nix
+++ users/phundrak/host/gampo.nix
@@ -1,11 +1,10 @@
 {config, ...}: {
   imports = [../home.nix];
   home = {
-    cli.nh.flake = "${config.home.homeDirectory}/nixos";
+    cli.nh.flake = "${config.home.homeDirectory}/.dotfiles";
     desktop.hyprland.host = "gampo";
     phundrak.sshKey = {
       content = builtins.readFile ../../../keys/id_gampo.pub;
-      # file = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
     };
   };
 }
