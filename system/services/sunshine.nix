b7b18f664e205ffe3ec744eb0ad1cae82025e0a1
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-09-03 11:45:12 +0200

    feat(sunshine): customise apps available in Sunshine

--- system/services/sunshine.nix
+++ system/services/sunshine.nix
@@ -16,5 +16,33 @@ in {
     capSysAdmin = true;
     openFirewall = true;
     settings.sunshine_name = config.mySystem.networking.hostname;
+    applications.apps = [
+      {
+        name = "Desktop";
+        image-path = "desktop.png";
+      }
+      {
+        name = "Low Res Desktop";
+        image-path = "desktop.png";
+      }
+      {
+        name = "Steam Big Picture";
+        detached = ["setsid steam steam://open/bigpicture"];
+        prep-cmd = {
+          do = "";
+          undo = "setsid steam steam://close/bigpicture";
+        };
+        image-path = "steam.png";
+      }
+      {
+        name = "OpenTTD";
+        cmd = "openttd";
+        image-path = "/home/phundrak/.config/sunshine/covers/igdb_18074.png";
+      }
+      {
+        name = "OpenMW";
+        cmd = "openmw";
+      }
+    ];
   };
 }
