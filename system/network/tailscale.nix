ffa6af675d6f36976f6839a2c2ceeabc16e872d4
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-09-03 11:45:12 +0200

    feat(tailscale): make tailscale togglable, defaults to enabled

--- system/network/tailscale.nix
+++ system/network/tailscale.nix
@@ -1,5 +1,16 @@
 {
-  services.tailscale = {
-    enable = true;
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.mySystem.network.tailscale;
+in {
+  options.mySystem.network.tailscale = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+    };
   };
+  config.services.tailscale.enable = cfg.enable;
 }
