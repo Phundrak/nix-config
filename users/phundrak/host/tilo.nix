0b01ed04f5bedd3fe84c2506bc86a2853e0790af
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-09-03 11:45:12 +0200

    style(home/tilo): better format file

--- users/phundrak/host/tilo.nix
+++ users/phundrak/host/tilo.nix
@@ -1,10 +1,7 @@
-{config, ...}: {
+{
   imports = [../light-home.nix];
   home = {
     cli.nh.flake = "/tank/phundrak/nixos";
-    phundrak.sshKey = {
-      content = builtins.readFile ../../../keys/id_tilo.pub;
-      # file = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
-    };
+    phundrak.sshKey.content = builtins.readFile ../../../keys/id_tilo.pub;
   };
 }
