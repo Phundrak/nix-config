5a5730a840ca5eec47e54b44b555498b1dba6ef0
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-11-01 18:22:04 +0100

    feat(qemu): improve QEMU configuration

--- system/dev/qemu.nix
+++ system/dev/qemu.nix
@@ -7,11 +7,25 @@
 with lib; let
   cfg = config.mySystem.dev.qemu;
 in {
-  options.mySystem.dev.qemu.enable = mkEnableOption "Enable QEMU";
+  options.mySystem.dev.qemu = {
+    enable = mkEnableOption "Enable QEMU";
+    users = mkOption {
+      type = types.listOf types.str;
+      default = ["phundrak"];
+      example = ["user1" "user2"];
+    };
+  };
   config = mkIf cfg.enable {
+    programs.virt-manager.enable = true;
+    users.groups.libvirtd.members = cfg.users;
+    virtualisation = {
+      libvirtd.enable = true;
+      spiceUSBRedirection.enable = true;
+    };
     environment.systemPackages = with pkgs; [
       qemu
-      virt-manager
+      quickemu
+      swtpm
     ];
     systemd.tmpfiles.rules = ["L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware"];
     boot.binfmt.emulatedSystems = ["aarch64-linux"];
