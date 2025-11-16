299d08a16a3ceaeae6ba2c2fb97106fbce66cea4
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-11-16 15:29:21 +0100

    feat(steam): better steam installation

--- system/packages/steam.nix
+++ system/packages/steam.nix
@@ -17,6 +17,20 @@ in {
         localNetworkGameTransfers.openFirewall = true;
         gamescopeSession.enable = true;
         extraCompatPackages = [pkgs.proton-ge-bin];
+        package = pkgs.steam.override {
+          extraEnv = {
+            MANGOHUD = true;
+            OBS_VKCAPTURE = true;
+            RADV_TEX_ANISO = 16;
+          };
+          extraLibraries = p: with p; [atk];
+          extraPkgs = pkgs:
+            with pkgs; [
+              qt5.qtmultimedia
+              qt5.qtbase
+              libpulseaudio
+            ];
+        };
       };
       gamescope = {
         enable = true;
