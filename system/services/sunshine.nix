efa9be431413d66cba13fd8273c3b0baafb7691b
Lucien Cartier-Tilet <lucien@phundrak.com>
2026-02-24 02:02:59 +0100

    fix(sunshine): automatically set screen resolution for sunshine

--- system/services/sunshine.nix
+++ system/services/sunshine.nix
@@ -21,38 +21,47 @@ in {
       system_tray = "enabled";
       output_name = 1;
     };
-    applications.apps = [
+    applications.apps = let
+      defaultPrep = [
+        {
+          do = "sh -c \"hyprctl -i 0 keyword monitor \\\"DP-2,\${SUNSHINE_CLIENT_WIDTH}x\${SUNSHINE_CLIENT_HEIGHT}@\${SUNSHINE_CLIENT_FPS},0x0,1\\\"\"";
+          undo = "sh -c \"hyprctl -i 0 keyword monitor 'DP-2,2560x1080@60,0x0,1,transform,1'\"";
+        }
+      ];
+    in [
       {
         name = "Desktop";
         image-path = "desktop.png";
+        prep-cmd = defaultPrep;
       }
       {
         name = "Low Res Desktop";
         image-path = "desktop.png";
+        prep-cmd = defaultPrep;
       }
       {
         name = "Steam Big Picture";
         detached = ["setsid steam steam://open/bigpicture"];
-        prep-cmd = {
-          do = "";
-          undo = "setsid steam steam://close/bigpicture";
-        };
+        prep-cmd = defaultPrep;
         image-path = "steam.png";
       }
       {
         name = "OpenTTD";
         cmd = "openttd";
         image-path = "/home/phundrak/.config/sunshine/covers/igdb_18074.png";
+        prep-cmd = defaultPrep;
       }
       {
         name = "OpenMW";
         cmd = "openmw";
         image-path = "/home/phundrak/.config/sunshine/covers/igdb_24775.png";
+        prep-cmd = defaultPrep;
       }
       {
         name = "Vintage Story";
         cmd = "flatpak run at.vintagestory.VintageStory";
         image-path = "/home/phundrak/.config/sunshine/covers/igdb_69547.png";
+        prep-cmd = defaultPrep;
       }
     ];
   };
